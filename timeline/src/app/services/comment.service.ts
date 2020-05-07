94b9f17588120e336babc794507c66eb3e6c5c84
nayara.pimentel <n.pimentel@vallourec.com>
2020-05-06 23:22:05 -0300

    Service para Comments

--- timeline/src/app/services/comment.service.ts
+++ timeline/src/app/services/comment.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { comments} from '../models/comments.model';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  private gitHubUrl = 'https://jsonplaceholder.typicode.com/comments/';
+
+  constructor( private http: HttpClient) { }
+
+  async getComments () {
+    const res = await this.http.get(this.gitHubUrl).toPromise();
+    console.log(res);
+    return res;  
+  }
+
+  async getCommentsById (id: number) {
+    const res = await this.http.get(this.gitHubUrl + id).toPromise();
+    console.log(res);
+    return res;  
+  }
+
+  async postCommentsById (comments) {
+    const res = await this.http.post(this.gitHubUrl,comments).toPromise();
+    console.log(res);
+    return res;  
+  }
+
+ 
 }
+
+
+
+// crie os métodos Get, GetById e Post utilizando Async Await. Os métodos
+// GetById deverão receber um id como parâmetro e os métodos posts deverão
+// receber um objeto do tipo específico de cada serviço como parâmetro.
